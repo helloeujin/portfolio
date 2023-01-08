69c902da2d727ad379922280cc07eb02a48d7531
Youjin <meggonagul@gmail.com>
2023-01-08 23:20:46 +0900

    interaction added

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import "./css/Portfolio.scss";
 
 const Portfolio = () => {
+  const [toggle, setToggle] = useState(false);
+
   const data = require("./data/data.json");
   // console.log(data);
+
+  const toggleDesc = (event) => {
+    setToggle((current) => !current);
+  };
+
   return (
     <div className="portfolio">
-      {data.map((project) => {
+      {data.map((project, i) => {
         return (
-          <div className="project">
+          <div className="project" key={"project" + i}>
             <div className="project-content">
               <div className="tag">{project.tag}</div>
-              <div className="hed">{project.hed}</div>
+              <div className="hed" onClick={toggleDesc}>
+                {project.hed}{" "}
+                <div className={toggle ? "rotateArrow arrow" : "arrow"}>
+                  &#x2192;{" "}
+                </div>{" "}
+              </div>
+              <div className={toggle ? "showDesc desc" : "desc"}>
+                {project.desc}
+              </div>
               <a href={project.url} target="_blank">
                 <img src={process.env.PUBLIC_URL + "/" + project.img} />
               </a>
